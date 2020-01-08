a9017f88b4b8e483ff9433243637c267492cefe0
Karthick Rajan <kaarthikrajann@gmail.com>
2020-01-08 10:47:45 +0530

    Adding Product Container

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,44 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../logo.svg';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
+import styled from "styled-components";
+import { Button } from "./Button";
 
 class Navbar extends Component {
-    state = {}
-    render() {
-        return (
-            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5 ">
-                <Link to='/'>
-                    <img src={logo} alt="Store" className="navbar-brand" />
-                </Link>
-                <ul className="navbar-nav align-items-center">
-                    <li className="nav-item ml-5">
-                        <Link to="/" className="nav-link">
-                            Products
-                        </Link>
-                    </li>
-                </ul>
-                <Link to="/cart" className="ml-auto">
-                    <button>
-                        <i className="fa fa-cart-plus"></i>
-                        My Cart
-                    </button>
-                </Link>
-            </nav>
-        );
-    }
+  state = {};
+  render() {
+    return (
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5 ">
+        <Link to="/">
+          <img src={logo} alt="Store" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              Products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <Button>
+            <span>
+              <i className="fa fa-cart-plus"></i>
+            </span>
+            My Cart
+          </Button>
+        </Link>
+      </NavWrapper>
+    );
+  }
 }
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`;
 
 export default Navbar;
