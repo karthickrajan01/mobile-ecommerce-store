b28f8ea1980d45499461bfc3d4261f259460d793
Karthick Rajan <kaarthikrajann@gmail.com>
2020-01-09 08:53:55 +0530

    Added Paypal Button

--- src/components/Cart/CartTotals.jsx
+++ src/components/Cart/CartTotals.jsx
@@ -1,33 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import PaypalButton from "./PaypalButton";
 
-export default function CartTotals({ value }) {
+export default function CartTotals({ value, history }) {
+  const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
 
-    const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
-
-    return (
-        <React.Fragment>
-            <div className="container">
-                <div className="row">
-                    <div className="col-10 mt-10 ml-sm-5 ml-md-auto col-sm-8 text-captitalize text-right">
-                        <Link className="btn btn-outline-danger text-uppercase mb-5 px-5" type="button" to="/" onClick={() => clearCart()}>
-                            clear cart
-                        </Link>
-                        <h5>
-                            <span className="text-title">subtotal:</span>
-                            <strong>$ {cartSubTotal}</strong>
-                        </h5>
-                        <h5>
-                            <span className="text-title">cartTax:</span>
-                            <strong>$ {cartTax}</strong>
-                        </h5>
-                        <h5>
-                            <span className="text-title">cartTotal:</span>
-                            <strong>$ {cartTotal}</strong>
-                        </h5>
-                    </div>
-                </div>
-            </div>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mt-10 ml-sm-5 ml-md-auto col-sm-8 text-captitalize text-right">
+            <Link
+              className="btn btn-outline-danger text-uppercase mb-5 px-5"
+              type="button"
+              to="/"
+              onClick={() => clearCart()}
+            >
+              clear cart
+            </Link>
+            <h5>
+              <span className="text-title">subtotal:</span>
+              <strong>$ {cartSubTotal}</strong>
+            </h5>
+            <h5>
+              <span className="text-title">cartTax:</span>
+              <strong>$ {cartTax}</strong>
+            </h5>
+            <h5>
+              <span className="text-title">cartTotal:</span>
+              <strong>$ {cartTotal}</strong>
+            </h5>
+            <PaypalButton
+              total={cartTotal}
+              clearCart={clearCart}
+              history={history}
+            ></PaypalButton>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
